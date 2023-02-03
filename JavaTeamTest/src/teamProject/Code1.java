7ee3456dc7b444fb4cc96cba1ecb72caf2cb0c6c
Dmtome <dmtome@naver.com>
2023-02-03 16:27:26 +0900

    dongmin commit

--- JavaTeamTest/src/teamProject/Code1.java
+++ JavaTeamTest/src/teamProject/Code1.java
@@ -3,7 +3,7 @@ package teamProject;
 public class Code1 {
 
 	public static void main(String[] args) {
-		System.out.println("team project1");
+		System.out.println("dongmin shin");
 		System.out.println("손준용");
 	}
 
